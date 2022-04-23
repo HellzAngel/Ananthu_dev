c02686cce411cc54e11ec5690e32712861013002
HellzAngel <itzmeanuhere01@gmail.com>
2022-04-24 04:48:41 +0530

    efect

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,68 @@
 <template>
-  <div class="nav-bar sticky top-0 z-50">
-        <nav class="w-full text-gray-700 bg-white dark:text-gray-200 dark:bg-gray-800">
-            <div class="flex flex-col max-w-screen-2xl mx-2 md:items-center md:justify-between md:flex-row md:px-3 md:ml-8">
-                <div class="p-4 flex flex-row items-center justify-between">
-                    <a class="text-lg font-nav font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark:text-gray-200 focus:outline-none" href="#/">Ananthu</a>
-                    <button class="md:hidden rounded-lg focus:outline-none" id="headlessui-disclosure-button-1" type="button" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
-                    </button>
-                </div>
-            </div>
-        </nav>
-    </div>
+  <div class="bg-gray-600">
+    <nav
+      class="
+        container
+        px-6
+        py-6
+        mx-auto
+        md:flex md:justify-between md:items-center
+      "
+    >
+      <div class="flex items-center justify-between">
+        <router-link
+          to="/"
+          class="
+            text-xl
+            font-semibold 
+            text-gray-100
+            md:text-2xl
+          "
+          >ANANTHU
+        </router-link>
+        <!-- Mobile menu button -->
+        <div @click="toggleNav" class="flex md:hidden">
+          <button
+            type="button"
+            class="
+              text-gray-100
+              hover:text-gray-400
+              focus:outline-none focus:text-gray-400
+            "
+          >
+            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+              <path
+                fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+      <ul
+        :class="showMenu ? 'flex' : 'hidden'"
+        class="
+          flex-col
+          mt-8
+          space-y-4
+          md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0
+        "
+      >
+        <li class="inline-block  rounded text-gray-100 hover:bg-gray-500 py-2 px-4 transition duration-700 delay-750 hover:delay-2">Projects</li>
+        <li class="inline-block  rounded text-gray-100 hover:bg-gray-500 py-2 px-4 transition duration-700 delay-750 hover:delay-2">About</li>
+      </ul>
+    </nav>
+  </div>
 </template>
+<script>
+import { ref } from 'vue';
+export default {
+  setup() {
+    let showMenu = ref(false);
+    const toggleNav = () => (showMenu.value = !showMenu.value);
+    return { showMenu, toggleNav };
+  },
+};
+</script>
