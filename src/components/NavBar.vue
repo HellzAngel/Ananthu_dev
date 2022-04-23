c7581bcc7b1004cfd9e31f6cc5656248dd54381e
HellzAngel <itzmeanuhere01@gmail.com>
2022-04-24 02:29:06 +0530

    tailwind

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="nav-bar sticky top-0 z-50">
+        <nav class="w-full text-gray-700 bg-white dark:text-gray-200 dark:bg-gray-800">
+            <div class="flex flex-col max-w-screen-2xl mx-2 md:items-center md:justify-between md:flex-row md:px-3 md:ml-8">
+                <div class="p-4 flex flex-row items-center justify-between">
+                    <a class="text-lg font-nav font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark:text-gray-200 focus:outline-none" href="#/">Ananthu</a>
+                    <button class="md:hidden rounded-lg focus:outline-none" id="headlessui-disclosure-button-1" type="button" aria-expanded="false"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7"></path></svg>
+                    </button>
+                </div>
+            <div class="hidden md:flex md:justify-end md:flex-row pb-4 md:pb-0 ml-3 mr-3 md:m-0 sm:items-stretch sm:justify-start">
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
