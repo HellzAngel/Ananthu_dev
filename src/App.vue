31c7cf204423c4962d8d8327728e93f1f9aa8504
HellzAngel <itzmeanuhere01@gmail.com>
2022-04-24 16:06:47 +0530

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import WelcomeItem from './components/WelcomeItem.vue'
 import NavBar from './components/NavBar.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <NavBar/>
+  <Footer/>
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer class="w-full 
+                    px-4
+                    py-4 
+                    bg-gray-600 
+                    fixed
+                    bottom-0"
+                    >
+        <div class="py-2 text-gray-500 text-sm mt-3 mx-auto text-center">
+            Designed and built with 
+            <span class="text-red-500">♥</span> by <a class="hover:underline text-gray-300" href="https://twitter.com/saleh_mirnezami">Ananthu A Nair</a>. Licensed under the MIT License.
+        </div>
+    </footer>
+
+</template>
