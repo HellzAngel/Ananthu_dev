26147c6db233e561607b97b9e6d426dfd45f401e
HellzAngel <itzmeanuhere01@gmail.com>
2022-04-25 04:26:34 +0530

    routes

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
 import WelcomeItem from './components/WelcomeItem.vue'
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
+import Project from './components/Project.vue'
 </script>
 <template>
   <NavBar/>
+  <Project/>
   <Footer/>
 </template>
---
+++ src/components/Project.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex flex-wrap">
+    <div class="w-full rounded-full">
+      <ul class="flex mb-0  list-none flex-wrap pt-3 pb-4 flex-row">
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(1)" v-bind:class="{'text-gray-600 bg-white': openTab !== 1, 'text-white bg-gray-600': openTab === 1}">
+            GitHub
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(2)" v-bind:class="{'text-gray-600 bg-white': openTab !== 2, 'text-white bg-gray-600': openTab === 2}">
+            Live
+          </a>
+        </li>
+      </ul>
+      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
+        <div class="px-4 py-5 flex-auto">
+          <div class="tab-content tab-space">
+            <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+              <p>
+                Collaboratively administrate empowered markets via
+              </p>
+            </div>
+            <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+              <p>
+                Completely synergize resource taxing relationships via
+                ce.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pink-tabs",
+  data() {
+    return {
+      openTab: 1
+    }
+  },
+  methods: {
+    toggleTabs: function(tabNumber){
+      this.openTab = tabNumber
+    }
+  }
+}
+</script>
