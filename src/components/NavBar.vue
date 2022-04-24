a14eda885d8f008f31eef951bd3cc72ccfeb8791
HellzAngel <itzmeanuhere01@gmail.com>
2022-04-24 15:31:45 +0530

    efects

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -50,14 +50,20 @@
           md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0
         "
       >
-        <li class="inline-block  rounded text-gray-100 hover:bg-gray-500 py-2 px-4 transition duration-700 delay-750 hover:delay-2">Projects</li>
-        <!-- <li class="inline-block  rounded text-gray-100 hover:bg-gray-500 py-2 px-4 transition duration-700 delay-750 hover:delay-2">About</li> -->
+        <li class="inline-block rounded-full text-gray-100 hover:bg-gray-500 py-2 px-4 transition duration-700 delay-750 hover:delay-2">Projects</li>
+        <li class="inline-block  rounded-full text-gray-100 hover:bg-gray-500 py-2 px-2 transition duration-700 delay-750 hover:delay-2" @click="toggleDarkMode">
+          <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd">
+            </path>
+          </svg>
+        </li>
       </ul>
     </nav>
   </div>
 </template>
 <script>
-import { ref } from 'vue';
+import { ref } from 'vue'; 
+
 export default {
   setup() {
     let showMenu = ref(false);
