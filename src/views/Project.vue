303c50d40e7d0510d1d66dd7c1b4ce526507fd60
HellzAngel <itzmeanuhere01@gmail.com>
2022-04-26 02:01:37 +0530

    project done

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -33,10 +33,33 @@
           <div class="tab-content tab-space">
             <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
             <div>
-                <ul class="grid grid-cols-3 gap-4">
-                  <li v-for="post in posts" v-bind:key="post" class="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                      {{post.name}}
-                      {{post.fork}}
+                <ul class="grid grid-cols-1 md:grid-cols-3 gap-4 m-10">
+                  <li v-for="post in posts" v-bind:key="post">
+                      <a href={{post.html_url}}></a>
+                      <div class="bg-white dark:bg-gray-800 rounded-lg shadow-lg hover:shadow-2xl transition duration-500">
+                        <div class="flex gap-6 justify-between p-4">
+                          <div>
+                            <h1 class="text-md text-gray-600 dark:text-gray-200 font-semibold mb-3">{{post.name}}</h1>
+                              <p class="text-gray-600 dark:text-gray-400 tracking-normal text-sm">
+                                {{post.description}}</p>
+                                </div>
+                                <div class="grid grid-cols-1 text-gray-600 space-y-6 text-sm">
+                                  <span class="fa fa-star flex hover:text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                      </svg>
+                                    <p class="pl-1 font-nav">
+                                   {{post.stargazers_count}}</p>
+                                  </span>
+                                  <span class="fa fa-code-fork flex hover:text-gray-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                      <path stroke-linecap="round" stroke-linejoin="round" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
+                                    </svg>
+                                    <p class="pl-1 font-nav">{{post.forks}}</p>
+                                    </span>
+                                </div>
+                              </div>
+                            </div>
                     </li>
                   </ul>
               
