d9f79be5335ccae5e181427315f4182ce3979392
HellzAngel <itzmeanuhere01@gmail.com>
2022-04-27 00:51:19 +0530

    home

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,8 +8,9 @@
                     >
         <div class="py-2 text-gray-500 font-mono text-sm mt-3 mx-auto text-center font-semibold">
             Made With 
-            <span class="text-red-500 h-4 w-4s font-semibold text-2xl">♥</span> By <a class="hover:underline text-gray-300 font-semibold" href="/">Ananthu A Nair</a>
-        </div>
+            <span class="text-red-500 text-2xl animate-bounce">❤️</span> By 
+            <a class="hover:underline text-gray-300 font-semibold" href="/">Ananthu A Nair</a>
+        </div> 
     </footer>
 
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,55 @@
 <template>
-<div class="m-6 mb-10 lg:m-10 lg:mt-8 lg:mb-0 lg:pt-2">
-    <p class="flex text-2xl lg:text-3xl font-mono  font-semibold" >Hi There!
-        <span class="animate-waving-hand">👋🏻
-        </span>
-    </p>      
-    <p class="font-body pt-2 text-xl font-bold font-mono">
-        Ananthu here,
-    </p>
-    <p class="font-body pt-2 font-bold font-mono text-sm ">
-         Full Stack Developer From INDIA
-    </p>
-</div>
+    <div class="m-6 mb-10 lg:m-10 lg:mt-8 lg:mb-0 lg:pt-2">
+        <p class="flex text-2xl lg:text-3xl font-mono  font-semibold dark:text-gray-500 text-gray-700" >Hi There!
+            <span class="animate-waving-hand">👋🏻
+            </span>
+        </p>      
+        <p class="font-body pt-2 text-xl font-bold font-mono dark:text-gray-500 text-gray-700">
+            Ananthu here,
+        </p>
+        <p class="font-body pt-2 font-bold font-mono text-sm dark:text-gray-500 text-gray-700">
+            Full Stack Developer From INDIA
+        </p>
+    </div>
+    <div class="m-7 mt-10 grid md:grid-cols-2 lg:m-10 lg:mt-6">
+        <div class="lg:pt-8">
+            <h1 class="font-mono font-semibold text-xl dark:text-gray-500 text-gray-700">
+                About me
+            </h1>
+            <p class="font-body pt-2 font-bold font-mono text-sm dark:text-gray-500 text-gray-700">
+               A self-motivated and organised professional with a 2 of experience in It Field, who wants to be associated with an organisation that provides career development opportunities as a Frontend Developer and an environment to enable me to contribute to its growth. 
+            </p>
+        </div>
+        <div class="lg:pt-8 m-4">
+            <div class="grid grid-cols-4 col-span-1 gap-x-1 gap-y-1">
+                <div class="tag hover:animate-pulse">
+                    <img src="../assets/gitlab.svg" class="w-20 h-20">
+                </div>                
+                <div class="tag hover:animate-pulse">
+                    <img src="../assets/html.svg" class="w-20 h-20">
+                </div>
+                <div class="tag hover:animate-pulse">
+                    <img src="../assets/logo.svg" class=" w-20 h-20">
+                </div>
+                <div class="tag hover:animate-pulse">
+                    <img src="../assets/py.svg" class="w-20 h-20">
+                </div>
+                <div class="tag hover:animate-pulse">
+                    <img src="../assets/js.svg" class=" w-20 h-20">
+                </div>
+                <div class="tag hover:animate-pulse">
+                    <img src="../assets/node.svg" class="w-20 h-20">
+                </div>
+                <div class="tag hover:animate-pulse">
+                    <img src="../assets/react.svg" class="w-20 h-20">
+                </div>
+                <div class="tag hover:animate-pulse">
+                    <img src="../assets/mongo.svg" class="w-20 h-20">
+                </div>
+                <div class="tag hover:animate-pulse">
+                    <img src="../assets/post.svg" class="w-20 h-20">
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
